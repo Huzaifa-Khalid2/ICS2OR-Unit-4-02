15e59214638c6219badce1d7a880354d3d935833
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2020-12-04 15:36:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+(button 
+onclick="document.getElementById"(food table).src='images/food.jpeg'");Clear table(button);
+(img id="food table" src="images/food.jpeg" style="width:100px");
+(button 
+onclick="document.getElementById"(food table).src='images/food.jpeg'"); put food back on table(/button); 
